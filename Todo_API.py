1f57dc7663b40fbe3d5b6c7d7ddc59daf48aac79
Miguel <miguel.goncalves.p.leite@gmail.com>
2026-08-14 18:41:12 +0100

    Implemented login validation and functionality.

--- Todo_API.py
+++ Todo_API.py
@@ -15,6 +15,11 @@ def validate_email(email):
             break
     return False
 
+def checkdb_for_email(email, cursor):
+
+    cursor.execute('SELECT rowid, email FROM users WHERE email = :email', {'email': email})
+    return cursor.fetchone()
+
 @app.route('/register', methods=["POST"])
 def register():
 
@@ -24,16 +29,14 @@ def register():
     ph = PasswordHasher()
 
     try:
-        if validate_email(data['email']) is not True:
+        if validate_email(data['email']) is False:
             return jsonify({'error': 'invalid email.'}), 400
 
-        cursor.execute('SELECT * FROM users WHERE email = :email', {'email':data['email']})
-
-        if cursor.fetchone() == data['email']:
+        if checkdb_for_email(data['email'], cursor) is not None:
             return jsonify({'error': 'email already registered.'}), 400
 
         hashed_pass = ph.hash(data['password'])
-        cursor.execute('INSERT into users VALUES(:name, :email, :password)', {'name':data['name'], 'email':data['email'], 'password':hashed_pass})
+        cursor.execute('INSERT into users(name, email, password) VALUES(:name, :email, :password)', {'name':data['name'], 'email':data['email'], 'password':hashed_pass})
         con.commit()
 
     except sqlite3.Error as e:
@@ -49,8 +52,44 @@ def register():
 @app.route('/login', methods=['POST'])
 def login():
 
-    
+    data = request.get_json()
+    con = sqlite3.connect('registered_users.db')
+    cursor = con.cursor()
+    email_check = checkdb_for_email(data['email'], cursor)
 
+    try:
+        if email_check is not None:
+
+            cursor.execute('SELECT rowid, password FROM users WHERE rowid= :rowid', {'rowid': email_check[0]})
+            password = cursor.fetchone()[0]
+
+            if password == data['password']:
+                login_token = secrets.token_urlsafe()
+                return jsonify({'token': login_token}), 200
+
+            else:
+                return jsonify({'error': 'invalid password.'}), 400
+
+        else: 
+            return jsonify({'error': 'email not found.'}), 400
+
+    except sqlite3.Error as e:
+        return jsonify({'error': str(e)}), 400
+
+    finally:
+        con.close()
+
+
+@app.route('/todos', methods=['POST'])
+def to_dos():
+
+    data = request.get_json()
+    con = sqlite3.connect('registered_users.db')
+    cursor = con.cursor()
+
+    try:
+
+        if data['token'] ==  
     
 
 
--- db_init.py
+++ db_init.py
@@ -4,5 +4,7 @@ con = sqlite3.connect('registered_users.db')
 cursor = con.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS users(name TEXT, email TEXT, password TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS todo(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS tokens(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)')
 con.commit()
 con.close()
